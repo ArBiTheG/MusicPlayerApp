c98607b10bdfed0369978daa4f6b24ae2a4b89aa
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-20 10:26:37 +0300

    Добавлен интерфейс музыкального плеера

---
+++ MusicPlayerApp/Services/IMusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp.Services
+{
+    public interface IMusicPlayer
+    {
+        void Play();
+        void Pause();
+        void Stop();
+        void Seek(TimeSpan position);
+        bool IsPlaying { get; }
+        TimeSpan Position { get; }
+        TimeSpan Duration { get; }
+        float Volume { get; set; }
+    }
+}
