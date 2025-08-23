377b152b916095b4cc3ac50c923ceac967585ca3
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-23 11:46:12 +0300

    Добавлена библиотека для работы с метаданными музыкального файла и создан класс для работы с ним

---
+++ MusicPlayerApp/Services/IMusicMetadata.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp.Services
+{
+    public interface IMusicMetadata
+    {
+        string Title { get; }
+        string[] Artist { get; }
+        string Album { get; }
+        string[] Genres { get; }
+        uint Year { get; }
+        byte[] ImageBytes { get; }
+    }
+}
---
+++ MusicPlayerApp/Services/MusicMetadata.cs
@@ -0,0 +1,53 @@
+﻿using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib;
+using File = TagLib.File;
+
+namespace MusicPlayerApp.Services
+{
+    public class MusicMetadata : IMusicMetadata
+    {
+        File _file;
+        public MusicMetadata(string path)
+        {
+            _file = File.Create(path);
+        }
+        public string Title 
+        { 
+            get => _file.Tag.Title; 
+        }
+        public string[] Artist
+        {
+            get => _file.Tag.Performers;
+        }
+        public string Album
+        {
+            get => _file.Tag.Album;
+        }
+        public string[] Genres 
+        {
+            get => _file.Tag.Genres;
+        }
+        public uint Year
+        {
+            get => _file.Tag.Year;
+        }
+        public byte[] ImageBytes
+        {
+            get {
+                IPicture? picture = _file.Tag.Pictures?.FirstOrDefault();
+                if (picture != null)
+                {
+                    return picture.Data.Data;
+                }
+                return [];
+            }
+        }
+
+    }
+}
