4097bc57dd20b3eab688d7539f77edbd08871273
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-23 15:16:33 +0300

    Добавлена стандартная привязка данных

--- MusicPlayerApp/ViewModels/HandleViewModel.cs
+++ MusicPlayerApp/ViewModels/HandleViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MusicPlayerApp.Models;
+using MusicPlayerApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,27 @@ using System.Threading.Tasks;
 
 namespace MusicPlayerApp.ViewModels
 {
-    public class HandleViewModel: ViewModelBase
+    public partial class HandleViewModel: ViewModelBase
     {
+        [ObservableProperty]
+        private Music? _music;
+
+        private MusicService? _musicService;
+        private IMusicPlayer? _musicPlayer;
+
+        public HandleViewModel()
+        {
+
+        }
+
+        public HandleViewModel(MusicService musicService, IMusicPlayer musicPlayer)
+        {
+            _musicService = musicService;
+            _musicPlayer = musicPlayer;
+
+            Music = musicService.Open(@"Skillet_-_Hero_47950055.mp3");
+        }
+
         public string Greeting { get; } = "Welcome to Avalonia!";
     }
 }
