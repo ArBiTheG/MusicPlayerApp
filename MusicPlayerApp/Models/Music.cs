1d638e2ea56a0798963e9ace52b89aedf5939ebb
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-23 11:44:40 +0300

    Добавлена модель музыкального файла

---
+++ MusicPlayerApp/Models/Music.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp.Models
+{
+    public class Music
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string[] Artist { get; set; }
+        public string Album { get; set; }
+        public string CoverUrl { get; set; }
+
+    }
+}
