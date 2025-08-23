49e12d084a8917e8779b9594ed8cd797efc656e1
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-23 11:48:23 +0300

    Добавлен загрузчик музыки

---
+++ MusicPlayerApp/Services/IMusicLoader.cs
@@ -0,0 +1,14 @@
+﻿using MusicPlayerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp.Services
+{
+    public interface IMusicLoader
+    {
+        Music Load(string path);
+    }
+}
---
+++ MusicPlayerApp/Services/LocalMusicLoader.cs
@@ -0,0 +1,30 @@
+﻿using MusicPlayerApp.Models;
+using MusicPlayerApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp.Services
+{
+    public class LocalMusicLoader: IMusicLoader
+    {
+        protected readonly Func<string, IMusicMetadata> CreateMetadata;
+        public LocalMusicLoader(Func<string, IMusicMetadata> createMetadata)
+        {
+            CreateMetadata = createMetadata;
+        }
+        public Music Load(string path)
+        {
+            IMusicMetadata metadata = CreateMetadata(path);
+            return new Music()
+            {
+                Path = path,
+                Name = metadata.Title,
+                Album = metadata.Album,
+                Artist = metadata.Artist
+            };
+        }
+    }
+}
