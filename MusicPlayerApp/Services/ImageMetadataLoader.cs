31c9c83a10cecef0942dee86732f453897902533
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-23 11:47:10 +0300

    Добавлен загрузчик изображений

---
+++ MusicPlayerApp/Services/ImageMetadataLoader.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp.Services
+{
+    public class ImageMetadataLoader: IImageLoader
+    {
+        public byte[] LoadBytes(string path)
+        {
+            IMusicMetadata metadata = new MusicMetadata(path);
+            return metadata.ImageBytes;
+        }
+    }
+}
