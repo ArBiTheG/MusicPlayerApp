76f958975102295681a49bcc4503a051392f559c
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-24 11:50:03 +0300

    Добавлена функциональность командам

--- MusicPlayerApp/ViewModels/HandleViewModel.cs
+++ MusicPlayerApp/ViewModels/HandleViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using MusicPlayerApp.Models;
 using MusicPlayerApp.Services;
 using System;
@@ -11,16 +12,21 @@ namespace MusicPlayerApp.ViewModels
 {
     public partial class HandleViewModel: ViewModelBase
     {
-        [ObservableProperty]
-        private MusicViewModel? _currentMusic;
-
         private MusicService? _musicService;
         private IMusicPlayer? _musicPlayer;
         private IMusicImageLoader? _musicImageLoader;
 
+        [ObservableProperty]
+        private MusicViewModel? _currentMusic;
+
+        [ObservableProperty]
+        private int _progressMusic;
+
+        [ObservableProperty]
+        private int _volumeMusic;
+
         public HandleViewModel()
         {
-
         }
 
         public HandleViewModel(MusicService musicService, IMusicPlayer musicPlayer, IMusicImageLoader musicImageLoader)
@@ -30,17 +36,76 @@ namespace MusicPlayerApp.ViewModels
             _musicImageLoader = musicImageLoader;
         }
 
-        private void AddMusic(string path)
+        [RelayCommand]
+        private async Task LoadAsync()
         {
             if (_musicService == null)
                 return;
 
-            var music = _musicService.Open(path);
+            if (_musicPlayer == null)
+                return;
 
+            Music music = _musicService.Open(@"Skillet_-_Hero_47950055.mp3");
+            _musicPlayer.Load(music.Path);
             CurrentMusic = new MusicViewModel(music, _musicImageLoader);
-            CurrentMusic.LoadCover();
+            await CurrentMusic.LoadCover();
         }
 
-        public string Greeting { get; } = "Welcome to Avalonia!";
+
+        [RelayCommand]
+        private async Task AddMusicAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private async Task PlayPauseMusicAsync()
+        {
+            if (_musicPlayer == null)
+                return;
+
+            if (!_musicPlayer.IsPlaying)
+                _musicPlayer.Play();
+            else
+                _musicPlayer.Pause();
+
+            await Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private async Task StopMusicAsync()
+        {
+            if (_musicPlayer == null)
+                return;
+
+            _musicPlayer.Stop();
+
+            await Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private async Task PreviousMusicAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private async Task NextMusicAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        partial void OnProgressMusicChanged(int value)
+        {
+            if (_musicPlayer == null)
+                return;
+            _musicPlayer.Position = (float)value/100;
+        }
+        partial void OnVolumeMusicChanged(int value)
+        {
+            if (_musicPlayer == null)
+                return;
+            _musicPlayer.Volume = value;
+        }
     }
 }
