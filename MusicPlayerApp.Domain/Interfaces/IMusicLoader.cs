1fd3b02ce38591bd4ecc5186647402efc85c1036
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-27 20:17:39 +0300

    Добавлены интерфейсы для Loader'ов

---
+++ MusicPlayerApp.Domain/Interfaces/IMusicLoader.cs
@@ -0,0 +1,14 @@
+﻿using MusicPlayerApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp.Domain.Interfaces
+{
+    public interface IMusicLoader
+    {
+        Music Load(string path);
+    }
+}
