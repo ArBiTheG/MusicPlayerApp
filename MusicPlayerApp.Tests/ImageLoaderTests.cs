513ef192940d32855121e4a47a63cf7c65d59b03
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-28 11:45:09 +0300

    Добавлены юнит тесты для проекта

---
+++ MusicPlayerApp.Tests/ImageLoaderTests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using MusicPlayerApp.Domain.Interfaces;
+using MusicPlayerApp.Infrastructure.Loaders;
+using TagLib.Id3v2;
+using Xunit.Abstractions;
+
+namespace MusicPlayerApp.Tests
+{
+    public class ImageLoaderTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public ImageLoaderTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void Load_SkilletMusic_NotEmpty()
+        {
+            long excepted = 0;
+            long actual = 0;
+
+            IImageLoader loader = new TagLibImageLoader();
+            using (var stream = loader.Load(@"C:\Users\Daniil\Music\Hero.mp3"))
+            {
+                actual = stream.Length;
+            }
+            output.WriteLine($"Bytes: {actual.ToString()}");
+
+            Assert.NotEqual(excepted, actual);
+        }
+    }
+}
---
+++ MusicPlayerApp.Tests/MusicLoaderTests.cs
@@ -0,0 +1,32 @@
+﻿using MusicPlayerApp.Domain.Interfaces;
+using MusicPlayerApp.Infrastructure.Loaders;
+using Xunit.Abstractions;
+
+namespace MusicPlayerApp.Tests
+{
+    public class MusicLoaderTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public MusicLoaderTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void Load_SkilletMusic_Title()
+        {
+            string excepted = "Hero";
+            string actual = string.Empty;
+
+            IMusicLoader loader = new TagLibMusicLoader();
+
+            var music = loader.Load(@"C:\Users\Daniil\Music\Hero.mp3");
+            actual = music.Title;
+
+            output.WriteLine($"Title: {music.Title}");
+
+            Assert.Equal(excepted, actual);
+        }
+    }
+}
