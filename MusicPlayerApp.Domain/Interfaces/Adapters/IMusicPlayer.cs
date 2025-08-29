ce3d4de1926230183cf190bbd2b522e3ce611871
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-29 12:31:21 +0300

    Добавлена файл музыкального плеера

---
+++ MusicPlayerApp.Domain/Interfaces/Adapters/IMusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp.Domain.Interfaces.Adapters
+{
+    public interface IMusicPlayer: IDisposable
+    {
+        void Open(string path);
+        bool Play();
+        void Pause();
+        void Stop();
+    }
+}
---
+++ MusicPlayerApp.Infrastructure/Adapters/VlcMusicPlayer.cs
@@ -0,0 +1,52 @@
+﻿using LibVLCSharp.Shared;
+using MusicPlayerApp.Domain.Interfaces;
+using MusicPlayerApp.Domain.Interfaces.Adapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp.Infrastructure.Adapters
+{
+    public class VlcMusicPlayer : IMusicPlayer
+    {
+        private const string NO_VIDEO = ":no-video";
+
+        private readonly MediaPlayer _mediaPlayer;
+        private readonly LibVLC _libVLC;
+
+        private bool _disposed;
+
+        public VlcMusicPlayer(MediaPlayer mediaPlayer, LibVLC libVLC)
+        {
+            _mediaPlayer = mediaPlayer;
+            _libVLC = libVLC;
+        }
+
+        public void Open(string path)
+        {
+            using (var media = new Media(_libVLC, new Uri(path), NO_VIDEO))
+            {
+                _mediaPlayer.Media = media;
+            }
+        }
+
+        public bool Play() => _mediaPlayer.Play();
+
+        public void Pause() => _mediaPlayer.Pause();
+
+        public void Stop() => _mediaPlayer.Stop();
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            _disposed = true;
+
+            _mediaPlayer.Dispose();
+            _libVLC.Dispose();
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
