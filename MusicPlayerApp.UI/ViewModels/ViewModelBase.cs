474311ed58609b57a16aa4044650ae5cf28f53de
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-08-31 16:51:55 +0300

    Добавлен слой UI

---
+++ MusicPlayerApp.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MusicPlayerApp.UI.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ MusicPlayerApp.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MusicPlayerApp.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
